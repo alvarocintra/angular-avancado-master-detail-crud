f5dbf3ac28eb143f75e35ac77fdd8edf4da45ce0
Álvaro Cintra <alvarocintra@hotmail.com>
2019-08-20 14:36:58 -0300

    Adding categories in-memory-database

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,35 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDbService {
+    createDb() {
+        const categories = [
+            {
+                id: 1,
+                name: 'Lazer',
+                description: 'Cinema, parques, praia, etc'
+            },
+            {
+                id: 2,
+                name: 'Moradia',
+                description: 'Pagamentos de contas da casa'
+            },
+            {
+                id: 3,
+                name: 'Saúde',
+                description: 'Plano de Saúde e remédios'
+            },
+            {
+                id: 4,
+                name: 'Salário',
+                description: 'Recebimento de salário'
+            },
+            {
+                id: 5,
+                name: 'Freelas',
+                description: 'Trabalhos como freelancer'
+            }
+        ];
+
+        return { categories }
+    }
+}
