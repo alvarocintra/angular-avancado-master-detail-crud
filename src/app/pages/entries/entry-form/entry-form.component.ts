51b9db70405a1d84fd4db7b51262aee592fa6bd9
Álvaro Cintra <alvarocintra@hotmail.com>
2019-08-21 18:19:04 -0300

    improving BaseResourceService and EntryService and CategoryService

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -145,7 +145,7 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
   }
 
   private createEntry() {
-    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+    const entry: Entry = Entry.fromJson(this.entryForm.value);
 
     this.entryService.create(entry).subscribe(
       // tslint:disable-next-line: no-shadowed-variable
@@ -157,7 +157,7 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
   }
 
   private updateEntry() {
-    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+    const entry: Entry = Entry.fromJson(this.entryForm.value);
 
     this.entryService.update(entry).subscribe(
       // tslint:disable-next-line: no-shadowed-variable
--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -14,7 +14,7 @@ import { Observable } from 'rxjs';
 export class EntryService extends BaseResourceService<Entry> {
 
   constructor(protected injector: Injector, protected categoryService: CategoryService) {
-    super(injector, 'api/entries');
+    super(injector, 'api/entries', Entry.fromJson);
   }
 
   create(entry: Entry): Observable<Entry> {
@@ -33,19 +33,5 @@ export class EntryService extends BaseResourceService<Entry> {
         return super.update(entry);
       })
     );
-  }
-
-  // PROTECTED METHODS
-  protected jsonDataToResources(jsonData: any[]): Entry[] {
-    const entries: Entry[] = [];
-    jsonData.forEach(element => {
-      const entry = Object.assign(new Entry(), element);
-      entries.push(entry);
-    });
-    return entries;
-  }
-
-  protected jsonDataToResource(jsonData: any): Entry {
-    return Object.assign(new Entry(), jsonData);
   }
 }
