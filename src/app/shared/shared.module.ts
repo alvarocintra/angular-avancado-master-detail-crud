7ca6d81cf594f9dbe95298792fe837ac1162cd73
Álvaro Cintra <alvarocintra@hotmail.com>
2019-08-22 15:37:15 -0300

    adding breadcrumb component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BreadCrumbComponent
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     FontAwesomeModule
   ],
   exports: [
+    // shared modules
     CommonModule,
     ReactiveFormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+
+    // shared components
+    BreadCrumbComponent
   ]
 })
 export class SharedModule { }
